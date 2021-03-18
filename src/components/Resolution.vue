ce76933d0e9819852b79b79308783d39d8c0a121
Florian Magalhaes <m.f.pro@outlook.com>
2021-03-18 18:00:25 +0100

    Factorisation des fonctions

--- src/components/Resolution.vue
+++ src/components/Resolution.vue
@@ -4,13 +4,14 @@
       <h2>{{ resolution.title }}</h2>
       <tr v-for="college in colleges" :key="college.id">
         <td>{{ college.name }}</td>
-        <td v-for="vote in giveFor(resolution, college)" :key="vote.id">
+        <td v-for="vote in getVotesResultsOfThisCollege(resolution, college)" :key="vote.id">
           {{ vote.nbrVote }} ({{ vote.parts }}) -
         </td>
       </tr>
       <tr>
-        <td v-if="resolution.app === true">OKAY</td>
+        <td v-if="resolution.approved === true">OKAY</td>
       </tr>
+      <button v-on:click="check(resolution)">Check</button>
     </div>
   </div>
 </template>
@@ -27,69 +28,129 @@ export default {
     };
   },
   methods: {
-    giveFor(resolution, college) {
+    getMembersOfCollege(college) {
       let membersOfThisCollege = this.members.filter(
         (m) => m.collegeId === college.id
       );
-
+      return membersOfThisCollege;
+    },
+    getMembersIdOfCollege(membersOfThisCollege) {
       let membersIdOfThisCollege = [];
       membersOfThisCollege.forEach(function (m) {
         membersIdOfThisCollege.push(m.id);
       });
-
-      let votesForThisReso = this.votes.filter(
+      return membersIdOfThisCollege;
+    },
+    getVotesOfResolution(resolution) {
+      let votesForThisResolution = this.votes.filter(
         (v) => v.resolutionId === resolution.id
       );
-
-      let votesForThisCollege = [];
-      votesForThisReso.forEach(function (v) {
+      return votesForThisResolution;
+    },
+    getVotesByCollege(votesForThisResolution, membersIdOfThisCollege) {
+      let votesByThisCollege = [];
+      votesForThisResolution.forEach(function (v) {
         if (membersIdOfThisCollege.includes(v.memberId)) {
-          votesForThisCollege.push(v);
+          votesByThisCollege.push(v);
         }
       });
-
+      return votesByThisCollege;
+    },
+    getTotalPartsOfCollege(membersOfThisCollege) {
       let totalParts = 0;
       membersOfThisCollege.forEach(function (m) {
         totalParts += m.nbrParts;
       });
-
-      let votesResults = [
+      return totalParts;
+    },
+    getVotesResultsOfCollege(
+      votesByThisCollege,
+      membersOfThisCollege,
+      resolution,
+      totalPartsOfThisCollege
+    ) {
+      let votesResultsOfThisCollege = [
         { nbrVote: 0, parts: 0 },
         { nbrVote: 0, parts: 0 },
         { nbrVote: 0, parts: 0 },
       ];
 
-      votesForThisCollege.forEach(function (v) {
+      votesByThisCollege.forEach(function (v) {
         let member = membersOfThisCollege.find((m) => m.id === v.memberId);
         if (v.vote) {
-          votesResults[0].nbrVote += 1;
-          votesResults[0].parts += member.nbrParts;
+          votesResultsOfThisCollege[0].nbrVote += 1;
+          votesResultsOfThisCollege[0].parts += member.nbrParts;
+          resolution.agree.push(member.id);
         } else if (v.vote === null) {
-          votesResults[2].nbrVote += 1;
-          votesResults[2].parts += member.nbrParts;
+          votesResultsOfThisCollege[2].nbrVote += 1;
+          votesResultsOfThisCollege[2].parts += member.nbrParts;
+          resolution.none.push(member.id);
         } else {
-          votesResults[1].nbrVote += 1;
-          votesResults[1].parts += member.nbrParts;
+          votesResultsOfThisCollege[1].nbrVote += 1;
+          votesResultsOfThisCollege[1].parts += member.nbrParts;
+          resolution.disagree.push(member.id);
         }
       });
 
-      votesResults.forEach(function (v) {
-        v.parts = Math.round((v.parts / totalParts) * 100 * 100) / 100;
+      votesResultsOfThisCollege.forEach(function (v) {
+        v.parts =
+          Math.round((v.parts / totalPartsOfThisCollege) * 100 * 100) / 100;
       });
-
-      if (votesResults[0].parts >= 50 && resolution.app != false) {
-        resolution.app = true;
+      return votesResultsOfThisCollege;
+    },
+    checkIfResolutionIsApproved(votesResultsOfThisCollege, resolution) {
+      if (
+        votesResultsOfThisCollege[0].parts >= 50 &&
+        resolution.approved != false
+      ) {
+        resolution.approved = true;
       } else {
-        resolution.app = false;
+        resolution.approved = false;
       }
+    },
+    getVotesResultsOfThisCollege(resolution, college) {
+      let membersOfThisCollege = this.getMembersOfCollege(college);
 
+      let membersIdOfThisCollege = this.getMembersIdOfCollege(
+        membersOfThisCollege
+      );
 
+      let votesForThisResolution = this.getVotesOfResolution(resolution);
 
-      return votesResults;
+      let votesByThisCollege = this.getVotesByCollege(
+        votesForThisResolution,
+        membersIdOfThisCollege
+      );
+
+      let totalPartsOfThisCollege = this.getTotalPartsOfCollege(
+        membersOfThisCollege
+      );
+
+      let votesResultsOfThisCollege = this.getVotesResultsOfCollege(
+        votesByThisCollege,
+        membersOfThisCollege,
+        resolution,
+        totalPartsOfThisCollege
+      );
+
+      this.checkIfResolutionIsApproved(votesResultsOfThisCollege, resolution);
+
+      return votesResultsOfThisCollege;
+    },
+    check(resolution) {
+      console.log(resolution.agree);
+      console.log(resolution.disagree);
+      console.log(resolution.none);
     },
   },
   mounted() {
     this.resolutions = this.$resolutionsList;
+    this.$resolutionsList.forEach((resolution) => {
+      resolution.approved = true;
+      resolution.agree = [];
+      resolution.disagree = [];
+      resolution.none = [];
+    });
     this.colleges = this.$collegesList;
     this.votes = this.$votesList;
     this.members = this.$membersList;
